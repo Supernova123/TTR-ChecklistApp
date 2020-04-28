1bae26a6e6b6df2d56f22912c59388b7a37a5dab
Matthew Castronova <empnova12@aol.com>
2020-04-27 23:04:16 -0400

    Update router.js

--- server/router.js
+++ server/router.js
@@ -10,12 +10,17 @@ const router = (app) => {
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.get('/getToons', mid.requiresLogin, controllers.Toon.getToons);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.get('/about', mid.requiresSecure, mid.requiresLogout, controllers.About.aboutPage);
+  app.get('/changepassword', mid.requiresSecure, mid.requiresLogout, controllers.Password.changePasswordPage);
+  app.get('/checklist', mid.requiresLogin, controllers.Checklist.checklistPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+  app.get('/about', mid.requiresSecure, mid.requiresLogout, controllers.About.about);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  app.post('/changepassword', mid.requiresSecure, mid.requiresLogout, controllers.Account.changePassword);
+  app.post('/changepassword', mid.requiresSecure, mid.requiresLogout, controllers.Password.changePassword);
   app.get('/toons', mid.requiresLogin, controllers.Toon.toonPage);
   app.post('/toons', mid.requiresLogin, controllers.Toon.makeToon);
+  app.post('/checklist', mid.requiresLogin, controllers.Checklist.checklist);
 //  app.post('/checklist', mid.requiresLogin, controllers.Toon.postChecklist);
 //  app.post('/checklistred', mid.requiresLogin, controllers.Toon.checklistRed);
 //  app.post('/checklistgreen', mid.requiresLogin, controllers.Toon.checklistGreen); 
@@ -29,3 +34,4 @@ const router = (app) => {
 
 //Exports router functions
 module.exports = router;
+
